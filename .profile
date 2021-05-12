ded6d4a5d2b39e45c8a54e0aa484430b7a23557e
Ozgur Murat <zgrmrts@gmail.com>
2021-05-12 12:02:48 +0300

    npm path and settings added to profile
    setup_extra.sh cleaned

--- .profile
+++ .profile
@@ -1,4 +1,6 @@
 export PATH=${DOTFILES}/bin:${PATH}
 export PATH=${HOME}/.cargo/bin:${PATH}
 export PATH=${HOME}/go/bin:${PATH}
+export PATH=${HOME}/.npm-packages/bin:${PATH}
+npm config set prefix "${HOME}/.npm-packages"
 
--- setup_extra.sh
+++ setup_extra.sh
@@ -1,8 +1,3 @@
-# minicom setup & tty permissions
-sudo apt -y install minicom &&
-sudo usermod -a -G dialout ${USER} &&
-sudo cp ${DOTFILES}/setup/minicom/* /etc/minicom/. &&
-
 # install
 sudo apt -y install autoconf automake libtool &&
 sudo apt -y install make g++ gcc &&
@@ -15,10 +10,7 @@ sudo apt -y install cppreference-doc-en-html &&
 sudo apt -y install cowsay &&
 sudo apt -y install lolcat &&
 sudo apt -y install okteta &&
-sudo apt -y install virtualbox &&
 sudo apt -y install htop &&
-sudo apt -y install golang-go &&
 sudo apt -y install curl &&
-sudo apt -y install cargo &&
 
 echo extra installation complete
